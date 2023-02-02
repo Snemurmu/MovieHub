68ae4488f2dd4a295ef2457382e74b269e27550b
chester <chesterraedevera@gmail.com>
2023-02-02 09:10:42 -0500

    file added

---
+++ MovieHub/MovieHub/MovieHub/Login.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MovieHub
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        SqlConnection con;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-QAJII73\\SQLEXPRESS;Initial Catalog=Movie;Integrated Security=True";
+                con = new SqlConnection(connectionString);
+                con.Open();
+                string query = "Select count(*) from Account where username = @Username and password = @Password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("Username", username.Text);
+                cmd.Parameters.AddWithValue("Password", password.Text);
+
+                //Execute the query
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if ((count == 1) && (username.Text.Equals("chester") || username.Text.Equals("sneha")))
+                {
+                    MessageBox.Show("ADMIN Account Successfully Logged in!");
+                    con.Close();                    
+                    Movie movieAdmin = new Movie();
+                    movieAdmin.Show();
+                    this.Close();
+                }
+                else if (count == 1)
+                {
+                    MessageBox.Show("Account Successfully Logged in!");
+                    con.Close();                    
+                    MovieUser movieuser = new MovieUser();
+                    movieuser.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or Password!");
+                    con.Close();
+                    
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }   
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {            
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MovieHub/MovieHub/MovieHub/Register.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace MovieHub
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        SqlConnection con;
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = "Data Source=DESKTOP-QAJII73\\SQLEXPRESS;Initial Catalog=Movie;Integrated Security=True";
+                con = new SqlConnection(connectionString);
+                con.Open();
+                string query = "Insert into Account values(@Username, @Password, @First_name, @Last_name, @Date_of_birth, @Email)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("Username", username.Text);
+                cmd.Parameters.AddWithValue("Password", password.Text);
+                cmd.Parameters.AddWithValue("First_name", fName.Text);
+                cmd.Parameters.AddWithValue("Last_name", lName.Text);
+                cmd.Parameters.AddWithValue("Date_of_birth", dob.Text);
+                cmd.Parameters.AddWithValue("Email", email.Text);
+
+                //Execute the query
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Account Registered");
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {            
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
